d7c4ee78a722a78253efe582105c77bbb5b5ae16
Kristen Wilde <kristen.wilde@gmail.com>
2018-01-23 09:40:45 -0500

    object-oriented solution

--- 220/front_end_exercises/gui_apps/todos/todos.js
+++ 220/front_end_exercises/gui_apps/todos/todos.js
@@ -3,31 +3,40 @@ todo_items = [{id: 1, title: 'Homework'},
               {id: 3, title: 'Calling Mom'},
               {id: 4, title: 'Coffee with John'}];
 
-$(function() {
-  var todoTemplate = Handlebars.compile($('#template').html());
-  var currentId;
+var todoTemplate = Handlebars.compile($('#template').html());
 
-  function display_items() {
+var App = {
+  displayItems: function() {
     $('ul').html(todoTemplate(todo_items));
-  }
-
-  display_items();
-
-  $('ul').on('click', '.delete', function(e) {
-    $('.confirm').show();
-    currentId = $(this).parent().attr('id');
-    console.log(currentId);
-  });
-
-  $('#no').on('click', function(e) {
-    $('.confirm').hide();
-  });
-
-  $('#yes').on('click', function(e) {
-    $('.confirm').hide();
+  },
+  deleteItem: function(id) {
     todo_items = todo_items.filter(function(item) {
-      return String(item.id) !== currentId;
+      return item.id != id;
     })
-    display_items();
-  });
-});
+  },
+  bindHandlers: function() {
+    $('ul').on('click', '.delete', function(e) {
+      $('.confirm').fadeIn();
+      var id = $(this).parent().attr('id');
+      console.log(id);
+      $('#yes').attr('data-id', id);
+    });
+
+    $('#no').on('click', function(e) {
+      $('.confirm').fadeOut();
+    });
+
+    $('#yes').on('click', function(e) {
+      $('.confirm').fadeOut();
+      var id = $(this).attr('data-id');
+      App.deleteItem(id);
+      App.displayItems();
+    });
+  },
+  init: function() {
+    this.displayItems();
+    this.bindHandlers();
+  }
+}
+
+App.init();
