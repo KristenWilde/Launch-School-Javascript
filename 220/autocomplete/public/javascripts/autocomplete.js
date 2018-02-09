e0399be9948b4dfd8cebd25aa4f403a7617f962b
Kristen Wilde <kristen.wilde@gmail.com>
2018-02-09 11:18:08 -0500

    bug fix - when there are no matches for user input

--- 220/autocomplete/public/javascripts/autocomplete.js
+++ 220/autocomplete/public/javascripts/autocomplete.js
@@ -70,7 +70,7 @@ Autocomplete.prototype.valueChanged = function(event) {
     this.fetchMatches(value, function(matches){
       this.visible = true;
       this.matches = matches;
-      this.bestMatchIndex = 0;
+      this.bestMatchIndex = (matches.length > 0) ? 0 : null;
       this.selectedIndex = null;
       this.draw();
     }.bind(this));
@@ -103,6 +103,7 @@ Autocomplete.prototype.draw = function() { // renders the data in the listUI ele
   }
 
   if (this.bestMatchIndex != null) {
+    console.log('bestMatchIndex: ' + this.bestMatchIndex);
     var selected = this.matches[this.bestMatchIndex];
     this.overlay.textContent = selected.name;
   } else {
@@ -152,168 +153,4 @@ Autocomplete.prototype.createUI = function() {
 
   this.input.parentNode.appendChild(overlay);
   this.overlay = overlay;
-};
-
-var oldAutocomplete = {
-  init: function() {
-    this.input = document.querySelector('input');
-    this.url = '/countries?matching=';
-
-    this.listUI = null;
-    this.overlay = null;
-    
-    this.wrapInput();
-    this.createUI();
-
-    this.valueChanged = debounce(this.valueChanged.bind(this), 300);
-
-    this.bindEvents();
-    this.reset()
-  },
-
-  bindEvents: function() {
-    this.input.addEventListener('input', this.valueChanged.bind(this));
-    this.input.addEventListener('keydown', this.handleKeydown.bind(this));
-    this.listUI.addEventListener('click', this.mouseSelect.bind(this));
-  },
-
-  mouseSelect: function(e){
-    var li = e.target;
-    this.input.value = li.textContent;
-    this.reset();  
-  },
-
-  handleKeydown: function(event){
-    switch(event.key) {
-      case 'ArrowDown':
-        event.preventDefault();
-        if (this.selectedIndex === null || this.selectedIndex === this.matches.length - 1){
-          this.selectedIndex = 0;
-        } else {
-          this.selectedIndex += 1;
-        }
-        this.bestMatchIndex = null;
-        this.draw();
-        break
-      case 'ArrowUp':
-        event.preventDefault();
-        if (this.selectedIndex === null || this.selectedIndex === 0){
-          this.selectedIndex = this.matches.length - 1;
-        } else {
-          this.selectedIndex -= 1;
-        }
-        this.bestMatchIndex = null;
-        this.draw();
-        break
-      case 'Tab':
-        if (this.bestMatchIndex !== null) {
-          this.input.value = this.matches[this.bestMatchIndex].name;
-          event.preventDefault();
-        } 
-        this.reset();
-        break;
-      case 'Enter':
-        this.reset();
-        break;
-      case 'Escape':
-        this.input.value = this.previousValue;
-        this.reset();
-        break;
-    }
-  },
-
-  valueChanged: function(e) {
-    var value = this.input.value;
-    this.previousValue = value;
-
-    if (value.length > 0) {
-      this.fetchMatches(value, function(matches){
-        this.visible = true;
-        this.matches = matches;
-        this.bestMatchIndex = 0;
-        this.selectedIndex = null;
-        this.draw();
-      }.bind(this));
-    } else {
-      this.reset();
-    }
-  },
-
-  fetchMatches: function(query, callback) {
-    var request = new XMLHttpRequest();
-
-    request.addEventListener('load', function() {
-      callback(request.response)
-    }.bind(this));
-
-    request.open('GET', this.url + encodeURIComponent(query));
-    request.responseType = 'json';
-    request.send();
-  },
-
-  draw: function() { // renders the data in the listUI element.
-    var child;
-    while (child = this.listUI.lastChild) {
-      this.listUI.removeChild(child);
-    }
-
-    if (!this.visible) {
-      this.overlay.textContent = '';
-      return;
-    }
-
-    if (this.bestMatchIndex != null) {
-      var selected = this.matches[this.bestMatchIndex];
-      this.overlay.textContent = selected.name;
-    } else {
-      this.overlay.textContent = '';
-    }
-
-    this.matches.forEach(function(match, index) {
-      var li = document.createElement('li');
-      li.classList.add('autocomplete-ui-choice');
-
-      if (index === this.selectedIndex) {
-        li.classList.add('selected');
-        this.input.value = match.name;
-      }
-
-      li.textContent = match.name;
-      this.listUI.appendChild(li);
-    }, this);
-  },
-
-  reset: function() {
-    this.visible = false;
-    this.matches = [];  // the list of countries to display
-    this.bestMatchIndex = null;
-    this.selectedIndex = null;
-    this.prevousValue = null;
-
-    this.draw();
-  },
-                                    // Ask: Is there a reason to add these elements
-                                    // (wrapper div, ui list, and overlay)
-                                    // via javascript? Why not just put them in the erb?
-                                    // Is this just to show us how?
-  wrapInput: function() {
-    var wrapper = document.createElement('div');
-    wrapper.classList.add('autocomplete-wrapper');
-    this.input.parentNode.appendChild(wrapper);
-    wrapper.appendChild(this.input);
-  },
-
-  createUI: function() {
-    var listUI = document.createElement('ul');
-    listUI.classList.add('autocomplete-ui');
-    this.input.parentNode.appendChild(listUI);
-    this.listUI = listUI;
-
-    var overlay = document.createElement('div');
-    overlay.classList.add('autocomplete-overlay');
-    overlay.style.width = this.input.clientWidth + 'px';
-
-    this.input.parentNode.appendChild(overlay);
-    this.overlay = overlay;
-  }
 };
